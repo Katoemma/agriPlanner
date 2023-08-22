b70bc85344bc5d43e4001e76f930ab7676c6da2b
Katoemma <103442484+Katoemma@users.noreply.github.com>
2023-08-22 17:38:15 +0300

    modified the mobile menu

--- Admin/includes/header.php
+++ Admin/includes/header.php
@@ -79,34 +79,55 @@
         <!-- mobile menu -->
         <div class="hidden lg:hidden fixed top-4  w-11/12 md:w-3/4  mt-16 p-4 bg-green-600 z-50 border-1 border-red-500 rounded-xl" id="menu">
             <ul class="flex flex-col text-white">
-                <a href="dashboard.php" class="px-4 text-lg mb-4 py-2 hover:bg-green-900 hover:text-white rounded-lg ">
-                    <i class="fa fa-th-large"></i> Dashboard
-                </a>
+                <a href="index.php" class="px-4 text-lg mb-4 py-2 hover:bg-green-900 hover:text-white border-t">
+                    <i class="fa fa-th-large"></i> Dashboard</a>
+                <a href="farms.php" class="px-4  text-lg mb-4 py-2 hover:bg-green-900 hover:text-white border-y "><i class="fa fa-first-order"></i> Farms</a>
+
                 <div id="accordion-collapse" data-accordion="collapse">
-                    <h2 id="accordion-collapse-budget" class="text-white">
-                        <span type="button" class="flex items-center w-full justify-between px-4 text-lg mb-4 py-2 hover:bg-green-900 hover:text-white rounded-lg" data-accordion-target="#accordion-collapse-body-2" aria-expanded="false" aria-controls="accordion-collapse-body-1">
-                            <span class="text-lg hover:bg-green-900 hover:text-white rounded-lg "><i class="fa fa-money"></i> Budgets</span>
+                    <h2 id="accordion-collapse-heading-3" class="text-black">
+                        <span type="button" class="flex items-center w-full justify-between px-4 text-lg mb-4 py-2 hover:bg-green-900 hover:text-white rounded-lg" data-accordion-target="#accordion-collapse-body-3" aria-expanded="false" aria-controls="accordion-collapse-body-1">
+                            <span class="text-lg hover:bg-green-900 text-white rounded-lg "><i class="fa fa-money"></i> Crop Management</span>
                             <svg data-accordion-icon class="w-3 h-3 rotate-180 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5 5 1 1 5"/>
                             </svg>
                         </span>
                     </h2>
-                    <div id="accordion-collapse-body-2" class="hidden" aria-labelledby="accordion-collapse-budget">
-                        <div class="flex flex-col p-2 bg-gray-100 rounded-xl">
-                            <a href="dash_budgets.php" class="px-4  text-lg mb-4 py-2 bg-blue-500 text-white rounded-lg"><i class="fa fa-check"></i> Approved Budgets</a>
+                    <div id="accordion-collapse-body-3" class="hidden" aria-labelledby="accordion-collapse-heading-3">
+                        <div class="flex flex-col p-2 bg-gray-100">
+                            <a href="crop.php" class="px-4  text-lg mb-4 py-2 bg-blue-500 text-white rounded-lg"><i class="fa fa-check"></i> Approved Budgets</a>
                             <a href="dash_budgets.php" class="px-4 text-lg mb-4 py-2 bg-red-500 text-white rounded-lg"><i class="fa fa-spinner"></i> Pending Budgets</a>
                         </div>
                     </div>
                 </div>
-                <a href="dash_items.php" class="px-4  text-lg mb-4 py-2 hover:bg-green-900 hover:text-white rounded-lg "><i class="fa fa-list-alt"></i> Items</a>
-                <a href="#" class="px-4  text-lg mb-4 py-2 hover:bg-green-900 hover:text-white "><i class="fa fa-globe"></i> Departments</a>
-                <a href="dash_users.php" class="px-4  text-lg mb-4 py-2 hover:bg-green-900 hover:text-white rounded-lg"><i class="fa fa-users"></i> Users</a>
-                <a href="#" class="px-4  text-lg mb-4 py-2 hover:bg-green-900 hover:text-white rounded-lg"><i class="fa fa-cogs"></i>  Settings</a>
-                <hr class="border border-white rounded-full">
-                <div class="flex flex-col px-4 my-4">
-                    <a href="../logout.php" class="text-red-500 text-xl"><i class="fa fa-sign-out"></i> Logout</a>
+
+
+                <a href="dash_items.php" class="px-4  text-lg mb-4 py-2 hover:bg-green-900 hover:text-white  border-t"><i class="fa fa-list-alt"></i> Livestock Management</a>
+                <a href="#" class="px-4  text-lg mb-4 py-2 hover:bg-green-900 hover:text-white  border-t"><i class="fa fa-globe"></i> Equipment Tracking</a>
+                <a href="dash_users.php" class="px-4  text-lg mb-4 py-2 hover:bg-green-900 hover:text-white  border-t"><i class="fa fa-users"></i>Finances</a>
+                <a href="employees.php" class="px-4  text-lg mb-4 py-2 hover:bg-green-900 hover:text-white border-y"><i class="fa fa-briefcase"></i>Jobs</a>
+                
+                <div id="accordion-collapse" data-accordion="collapse">
+                    <h2 id="accordion-collapse-reports1" class="text-black">
+                        <span type="button" class="flex items-center w-full justify-between px-4 text-lg mb-4 py-2 hover:bg-green-900 hover:text-white rounded-lg" data-accordion-target="#accordion-collapse-reports2" aria-expanded="false" aria-controls="accordion-collapse-body-1">
+                            <span class="text-lg  text-white rounded-lg "><i class="fa fa-money"></i> Reports</span>
+                            <svg data-accordion-icon class="w-3 h-3 rotate-180 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5 5 1 1 5"/>
+                            </svg>
+                        </span>
+                    </h2>
+                    <div id="accordion-collapse-reports2" class="hidden" aria-labelledby="accordion-collapse-reports">
+                        <div class="flex flex-col p-2 bg-gray-100">
+                            <a href="dash_budgets.php" class="px-4  text-lg mb-4 py-2 bg-blue-500 text-white rounded-lg"><i class="fa fa-check"></i> Crops Reports</a>
+                            <a href="dash_budgets.php" class="px-4 text-lg mb-4 py-2 bg-red-500 text-white rounded-lg"><i class="fa fa-spinner"></i> Livestock Reports</a>
+                        </div>
+                    </div>
                 </div>
+
+                <a href="#" class="px-4  text-lg mb-4 py-2 hover:bg-green-900 hover:text-white rounded-lg border-y"><i class="fa fa-cogs"></i>  Settings</a>
             </ul>
+            <div class="flex flex-col px-4 mb-4">
+                <a href="../logout.php" class="text-gray-100 text-xl"><i class="fa fa-sign-out"></i> Logout</a>
+            </div>
         </div>
        
         <!-- main content -->
